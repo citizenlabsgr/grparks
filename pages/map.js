6d97974017950ac33367a28d8553c25a04314a41
joel6653 <software@startswitha.name>
2016-02-02 22:37:57 -0500

    stop script error in <IE11
    not guaranteed to make the page work in earlier IE, but worth a try

--- pages/map.js
+++ pages/map.js
@@ -2,7 +2,7 @@ loadJSON("https://raw.githubusercontent.com/friendlycode/gr-parks/gh-pages/parks
 
 function loadJSON(url, callback) {   
 	var xobj = new XMLHttpRequest();
-	xobj.overrideMimeType("application/json");
+	if (xobj.overrideMimeType) {xobj.overrideMimeType("application/json");}
 	xobj.open('GET', url, true);
 	xobj.onreadystatechange = function () {
 		if (xobj.readyState == 4 && xobj.status == "200") {callback(xobj.responseText);}
