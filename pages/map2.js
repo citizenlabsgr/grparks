4046c1ed6b402b798195ebcd2bd6c4de679d64d2
joel6653 <software@startswitha.name>
2016-03-06 10:15:24 -0500

    made javascript more object-oriented

--- pages/map2.js
+++ pages/map2.js
@@ -1,76 +1,75 @@
-// "constants"
-var CITY_BOUNDARY_DATA = "https://raw.githubusercontent.com/friendlycode/gr-parks/gh-pages/gr.geojson";
-var CITY_PARKS_DATA = "https://raw.githubusercontent.com/friendlycode/gr-parks/gh-pages/parks.geojson";
-var CITY_MAP_LAYER_COLOR = "yellow";
-var CITY_PARKS_LAYER_COLOR = "#ff7800";
+var constants = {
+	CITY_BOUNDARY_DATA_URL: "https://raw.githubusercontent.com/friendlycode/gr-parks/gh-pages/gr.geojson",
+	CITY_BOUNDARY_STYLE: {color: "yellow", weight: 1, opacity: 1, clickable: false},
+	CITY_CENTER: {lat: 42.9614844, lon: -85.6556833},
+	PARKS_DATA_URL: "https://raw.githubusercontent.com/friendlycode/gr-parks/gh-pages/parks.geojson",
+	PARKS_STYLE: {color: "#ff7800", weight: 1, opacity: 0.65, clickable: false}
+	};
 
-loadJSON(CITY_BOUNDARY_DATA, mapCity);
+ids = [];
+markers = [];
 
-function loadJSON(url, callback) {   
-	var xobj = new XMLHttpRequest();
-	if (xobj.overrideMimeType) {xobj.overrideMimeType("application/json");}
-	xobj.open('GET', url, true);
-	xobj.onreadystatechange = function () {
-		if (xobj.readyState == 4 && xobj.status == "200") {callback(xobj.responseText);}
-		};
-    xobj.send(null);  
- 	}
- 
-function mapCity(response) {
-	var city = JSON.parse(response);
-	var view = window.location.search.substring(1);
-	if (view == "") {view = "github.kedo1cp3";} else {view = "mapbox." + view;}
-	map = L.map("map", {center: [42.9614844, -85.6556833], zoom: 12});
-	L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-		attribution: 
-			"<a target='_blank' href='" +
-				"https://www.mapbox.com/about/maps/'>&copy; Mapbox</a> " +
-			"<a target='_blank' href='" +
-				"http://www.openstreetmap.org/copyright'>&copy; OpenStreetMap</a> " +
-			"<a target='_blank' href='" +
-				"https://www.mapbox.com/map-feedback/#/-85.596/42.997/14'><b>Improve this map</b></a>",
-		minZoom: 11,
-		maxZoom: 17,
-		id: view,
-		accessToken: "pk.eyJ1IjoiZ2l0aHViIiwiYSI6IjEzMDNiZjNlZGQ5Yjg3ZjBkNGZkZWQ3MTIxN2FkODIxIn0.o0lbEdOfJYEOaibweUDlzA"
-		}).addTo(map);
-	L.geoJson(city, {
-		style: {
-			color: CITY_MAP_LAYER_COLOR, 
-			weight: 1, 
-			clickable: false
-			}
-		}).addTo(map);
-	loadJSON(CITY_PARKS_DATA, mapParks);
+window.onload = function() {
+	
+	baseMap.init("map", constants.CITY_CENTER);
+	
+	var theCity = new customLayer(constants.CITY_BOUNDARY_DATA_URL, constants.CITY_BOUNDARY_STYLE);
+	theCity.getData();
+	
+	var theParks = new customLayer(constants.PARKS_DATA_URL, constants.PARKS_STYLE);
+	theParks.addMarker = makeMarker;
+	theParks.getData(makeParkList);
+	}
+
+baseMap = {
+	init: function(div, center) {
+		var view = window.location.search.substring(1);
+		if (view == "") {view = "github.kedo1cp3";} else {view = "mapbox." + view;}
+		this.map = L.map(div, {center: center, zoom: 12});
+		L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+			attribution: 
+				"<a target='_blank' href='" +
+					"https://www.mapbox.com/about/maps/'>&copy; Mapbox</a> " +
+				"<a target='_blank' href='" +
+					"http://www.openstreetmap.org/copyright'>&copy; OpenStreetMap</a> " +
+				"<a target='_blank' href='" +
+					"https://www.mapbox.com/map-feedback/#/-85.596/42.997/14'><b>Improve this map</b></a>",
+			minZoom: 11,
+			maxZoom: 17,
+			id: view,
+			accessToken: "pk.eyJ1IjoiZ2l0aHViIiwiYSI6IjEzMDNiZjNlZGQ5Yjg3ZjBkNGZkZWQ3MTIxN2FkODIxIn0.o0lbEdOfJYEOaibweUDlzA"
+			}).addTo(this.map);
+		},
+	}
+
+function customLayer(url, style) {
+	this.style = style;
+	this.url = url;
+	this.getData = function(callback) {
+		this.callback = callback;
+		var xobj = new XMLHttpRequest();
+		if (xobj.overrideMimeType) {xobj.overrideMimeType("application/json");}
+		xobj.open('GET', this.url, true);
+		var x = this;
+		xobj.onreadystatechange = function () {
+			if (xobj.readyState == 4 && xobj.status == "200") {x.draw(xobj.responseText);}
+			};
+	    xobj.send(null);  	
+		}
+	this.draw = function(response) {
+		var data = JSON.parse(response);
+		L.geoJson(data, {onEachFeature: this.addMarker, style: this.style}).addTo(baseMap.map);
+		if (!(this.callback === undefined)) {this.callback();}
+		}
+	function addMarker() {}
+	function callback() {}
 	}
 	
-function mapParks(response) {
-	parks = JSON.parse(response);
-	ids = [];
-	markers = [];
-	L.geoJson(parks, {
-		onEachFeature: makeMarker, 
-		style: {
-			color: CITY_PARKS_LAYER_COLOR, 
-			weight: 1, 
-			opacity: 0.65, 
-			clickable: false
-			}
-		}).addTo(map);
-	parks = undefined;
-	ids = undefined;
-	makeParkList();
-	}
-
 function makeMarker(feature, layer) {
 	if (ids.indexOf(feature.id) == -1 && feature.properties && feature.properties.name) {		
 		ids.push(feature.id);
 		if (!feature.properties.millage) {feature.properties.millage = "none";};
-		var thisMarker = L.marker(layer.getBounds().getCenter(), {riseOnHover: true}).addTo(map);
-		thisMarker.bindPopup(
-			h3ParkName(feature.properties), 
-			{closeButton: false, maxHeight: 300}
-			);
+		var thisMarker = L.marker(layer.getBounds().getCenter(), {riseOnHover: true}).addTo(baseMap.map);
 		thisMarker.on("click", function(e) {liPark(e.target.index).scrollIntoView()});
 		thisMarker.on("popupopen", function(e) {clickPark(e, true)});
 		thisMarker.on("popupclose", function(e) {clickPark(e), false});
@@ -80,13 +79,12 @@ function makeMarker(feature, layer) {
 			"acreage": feature.properties.acreage,
 			"pool": feature.properties.pool,
 			"millage": feature.properties.millage		
-			}
+			};
+		thisMarker.header = "<h3>" + thisMarker.park.name + "</h3>";
+		thisMarker.info = "description of improvements would go here";
+		thisMarker.bindPopup(thisMarker.header, {closeButton: false, maxHeight: 300});
 		markers.push(thisMarker);
 		}
-	}
-
-function h3ParkName(info) {
-	return ("<h3>" + info.name + "</h3>");
 	}
 
 function liPark(index) {
@@ -97,29 +95,25 @@ function clickPark(e, open) {
 	var index = e.target.index;
 	liPark(index).classList.toggle("highlight");
 	if (!open) {
-		thisMarker = markers[index];
-		thisMarker.setPopupContent(h3ParkName(thisMarker.park));
+		var thisMarker = markers[index];
+		thisMarker.setPopupContent(thisMarker.header);
 		}
 	}
 	
 function makeParkList() {
 	
 	markers.sort(function(a, b){return (a.park.name.toUpperCase() > b.park.name.toUpperCase()) ? 1 : -1;});
-	var longTextNeeded = true;
 	
 	for (i = 0; i < markers.length; i++) {
 		
-		thisMarker = markers[i];
+		var thisMarker = markers[i];
+		var thisPark = JSON.parse(JSON.stringify(thisMarker.park));
 		thisMarker.index = i;
 				
-		var li = document.createElement("li");
 		var a = document.createElement("a");
 		a.href = "javascript:pop(" + i + ");";
 		a.title = thisMarker.park.name;
 		
-		var needInfo = false;
-		
-		var thisPark = JSON.parse(JSON.stringify(thisMarker.park));
 		for (feature in thisPark) {
 			var p = document.createElement("p");
 			switch (feature) {
@@ -131,7 +125,6 @@ function makeParkList() {
 						p.innerHTML = "&nbsp;";
 						} 
 					else {
-						needInfo = true
 						p.innerHTML = 
 							"<a href='#' title='Details of improvements' onclick='moneyClicked(" + i +  ");'>" + thisPark[feature] + "&nbsp;<i class='fa fa-info-circle fa-lg'></i></a>";
 						;}
@@ -149,17 +142,9 @@ function makeParkList() {
 				}
 			a.appendChild(p);
 			} 
-		li.appendChild(a);
 		
-		if (needInfo) {
-			thisMarker.park.info = "description of improvements would go here";
-			if (longTextNeeded) {
-				thisMarker.park.info +=
-					", but this can easily be expanded to monster-size to fit a lot more text than can posibly go in one box of this size, run-on sentences and all, in the beginning, etc., and I guess I need to put even more stuff in here to prove my point, eh?";
-				longTextNeeded = false;
-				}
-			}
-		
+		var li = document.createElement("li");
+		li.appendChild(a);		
 		parklist.appendChild(li);
 		
 		}  
@@ -167,16 +152,16 @@ function makeParkList() {
 	}
 
 function moneyClicked(index) {
-	thisMarker = markers[index];
-	thisMarker.setPopupContent(h3ParkName(thisMarker.park) + thisMarker.park.info);
+	var thisMarker = markers[index];
+	thisMarker.setPopupContent(thisMarker.header + thisMarker.info);
 	pop(index);
 	}
 
 function pop(index) {
 	var thisMarker = markers[index];
 	var where = thisMarker.getLatLng();
-	var zoom = map.getZoom();
+	var zoom = baseMap.map.getZoom();
 	if (zoom < 15) {zoom = 15;}
-	map.setView(where, zoom, {animation: true});
+	baseMap.map.setView(where, zoom, {animation: true});
 	thisMarker.openPopup();
 	}
