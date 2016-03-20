a92923a26e78397686951d0799c88a63ab2c2536
joel6653 <software@startswitha.name>
2016-03-20 10:31:54 -0400

    enhance choropleth feature

--- pages/map2.js
+++ pages/map2.js
@@ -5,7 +5,8 @@ var constants = {
 	CITY_CENTER: {lat: 42.9614844, lon: -85.6556833},
 	
 	NEIGHBORHOOD_BOUNDARY_DATA_URL: "https://raw.githubusercontent.com/friendlycode/gr-parks/gh-pages/neighborhoods.geojson",
-	NEIGHBORHOOD_BOUNDARY_STYLE: {weight: 1, fill: false, fillOpacity: 0.5, clickable: false},
+	NEIGHBORHOOD_BOUNDARY_STYLE: {weight: 1, fill: true, fillOpacity: 0.65, clickable: true},
+	NEIGHBORHOOD_BOUNDARY_HIGHLIGHT: {weight: 5, fill: true, fillOpacity: 0.65, clickable: true},
 	
 	PARKS_DATA_URL: "https://raw.githubusercontent.com/friendlycode/gr-parks/gh-pages/parks.geojson",
 	PARKS_STYLE: {color: "#ff7800", weight: 1, opacity: 0.65, clickable: false},
@@ -14,19 +15,48 @@ var constants = {
 	MARKER_ICON_PATH: "images/marker-icons/",
 	
 	BASE_LAYERS: {"Default": "github.kedo1cp3", "Streets": "mapbox.streets", "Grayscale": "mapbox.light", "Emerald": "mapbox.emerald"}, // do not delete the first one ("Default")
-	
-	CHOROPLETH_COLOR: function(dollars) {
-	    return dollars > 1000000 ? '#54278f' :
-	           dollars >  750000 ? '#756bb1' :
-	           dollars >  500000 ? '#9e9ac8' :
-	           dollars >  250000 ? '#cbc9e2' :
-	           dollars >       0 ? '#f2f0f7' :
+
+	CHOROPLETH_MONEY: [0, 250000, 500000, 750000, 1000000],
+	CHOROPLETH_COLOR: function(amount) {
+	    return amount > 1000000 ? '#7a0177' :
+	           amount >  750000 ? '#c51b8a' :
+	           amount >  500000 ? '#f768a1' :
+	           amount >  250000 ? '#fbb4b9' :
+	           amount >       0 ? '#feebe2' :
 	           					   'transparent';
 		}
 	
 	};
 
+
 var ids = [], markers = [], neighborhoods = [], baseLayers = {}, overlayLayers = {}, markerClicked = false;
+
+var legend = L.control({position: 'bottomleft'});
+legend.onAdd = function(map) {
+	var div = L.DomUtil.create('div', 'info legend'),
+		labels = [],
+		from, to;
+	for (var i = 0; i < constants.CHOROPLETH_MONEY.length; i++) {
+		from = constants.CHOROPLETH_MONEY[i] + 1;
+		to = constants.CHOROPLETH_MONEY[i + 1];
+		labels.push(
+			'<i style="background:' + constants.CHOROPLETH_COLOR(from) + '"></i> $' +
+			from.toLocaleString("en-US") + (to ? '&ndash;' + to.toLocaleString("en-US") : '+'));
+		}
+	div.innerHTML = labels.join('<br>');
+	return div;
+	}
+
+var info = L.control({position: 'bottomright'});
+info.onAdd = function(map) {
+	this._div = L.DomUtil.create('div', 'info');
+	this.update();
+	return this._div;
+	};
+info.update = function(props) {
+	this._div.innerHTML = '<h4>Neighborhood Investment</h4>' +  (props ?
+		'<b>' + props.NEBRH + '</b>: $' + props.money.toLocaleString("en-US") : 'Hover over a neighbhood');
+	};
 
 // overlay layers are for the park markers and the neighborhoods
 // here we set up park markers layers first, neighborhoods gets added later because it is not shown by default
@@ -45,8 +75,12 @@ var theNeighborhoods = new customLayer(
 	constants.NEIGHBORHOOD_BOUNDARY_STYLE
 	);
 theNeighborhoods.onEachFeature = function(feature, layer) {
-	var thisNeighborhood = {shape: layer, name: feature.properties.NEBRH, dollars: 0};
-	neighborhoods.push(thisNeighborhood);
+	layer.on({
+		mouseover: highlightNeighborhood,
+		mouseout: resetNeighborhoodHighlight
+		});
+	feature.properties.money = 0;
+	neighborhoods.push(layer);
 	}
 theNeighborhoods.getData();
 
@@ -68,7 +102,10 @@ function isEverythingReady() {
 		overlayLayers["Neighborhoods"] = theNeighborhoods.layer;
 		L.control.layers(baseLayers, overlayLayers, {position: "topright", collapsed: false}).addTo(baseMap.map);
 		for (i = 0; i < neighborhoods.length; i++) {
-			neighborhoods[i].shape.setStyle({fill: true, fillColor: constants.CHOROPLETH_COLOR(neighborhoods[i].dollars)});
+			neighborhoods[i].setStyle({
+				fill: true, 
+				fillColor: constants.CHOROPLETH_COLOR(neighborhoods[i].feature.properties.money)
+				});
 			}
 		}
 	}
@@ -81,10 +118,8 @@ window.onload = function() {
 
 
 baseMap = {
-	
 	ready: false,
 	init: function(div, center) {
-		
 		var a = 
 			"<a target='_blank' href='" +
 				"https://www.mapbox.com/about/maps/'>&copy; Mapbox</a> " +
@@ -93,35 +128,38 @@ baseMap = {
 			"<a target='_blank' href='" +
 				"https://www.mapbox.com/map-feedback/#/-85.596/42.997/14'><b>Improve this map</b></a>",
 			u = "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZ2l0aHViIiwiYSI6IjEzMDNiZjNlZGQ5Yjg3ZjBkNGZkZWQ3MTIxN2FkODIxIn0.o0lbEdOfJYEOaibweUDlzA"
-	
 		for (key in constants.BASE_LAYERS) {
 			var layer = L.tileLayer(u, {id: constants.BASE_LAYERS[key], attribution: a, minZoom: 11, maxZoom: 17});
 			baseLayers[key] = layer;
 			}
-		
 		this.map = L.map(div, {center: center, zoom: 12, layers: baseLayers["Default"]});					
-		this.map.on("overlayadd", function(e) {
-			if (e.name != "Neighborhoods") {overlayLayers[e.name].eachLayer(function(layer) {liPark(layer.index).style.display = "block";});}
+		this.map.on({
+			overlayadd: function(e) {overlayChanged(e, true);},
+			overlayremove: function(e) {overlayChanged(e, false);}
 			})
-		this.map.on("overlayremove", function(e) {
-			if (e.name != "Neighborhoods") {overlayLayers[e.name].eachLayer(function(layer) {liPark(layer.index).style.display = "none";});}
-			})
-
 		this.ready = true;
-		
 		}
 	}
 
 
+function highlightNeighborhood(e) {
+	e.target.setStyle(constants.NEIGHBORHOOD_BOUNDARY_HIGHLIGHT);
+	info.update(e.target.feature.properties);
+	}
+
+
+function resetNeighborhoodHighlight(e) {
+	e.target.setStyle(constants.NEIGHBORHOOD_BOUNDARY_STYLE);
+	info.update();
+	}
+
+
 function customLayer(url, style) {
-	
-	this.layer = undefined;
+	this.layer;
 	this.ready = false;
 	this.style = style;
 	this.url = url;
-	
 	function onEachFeature() {}
-
 	this.getData = function() {
 		var xobj = new XMLHttpRequest();
 		if (xobj.overrideMimeType) {xobj.overrideMimeType("application/json");}
@@ -140,57 +178,47 @@ function customLayer(url, style) {
 			};
 	    xobj.send(null);  	
 		}
-		
 	return this;
-	
 	}
 
 
 function addMarker(feature, layer) {
-	
 	var newIcon = L.Icon.Default.extend({options: {}});
-	
 	if (ids.indexOf(feature.id) == -1 && feature.properties && feature.properties.name) {		
-	
 		ids.push(feature.id);
-				
 		var thisMarker = L.marker(layer.getBounds().getCenter(), {
 			icon: new newIcon({iconUrl: srcFromMarkerType(feature.properties.type)}), 
 			riseOnHover: true
 			}).addTo(overlayLayers[markerLabel(feature.properties.type.split(" ")[0])]);
-		thisMarker.on("click", function(e) {markerClicked = true});
-		thisMarker.on("popupopen", function(e) {clickPark(e, true)});
-		thisMarker.on("popupclose", function(e) {clickPark(e), false});
+		thisMarker.money = Number(
+			feature.properties.millage.replace(".00", "").replace("$", "").replace(",", "")
+			);
+		thisMarker.on({
+			click: function(e) {markerClicked = true;},
+			popupopen: function(e) {clickPark(e, true);},
+			popupclose: function(e) {clickPark(e, false);}
+			});
 		thisMarker.park = {
 			"name": feature.properties.name, 
 			"acreage": feature.properties.acreage,
 			"pool": feature.properties.pool,
 			"millage": feature.properties.millage		
 			};
-		thisMarker.dollars = Number(
-			feature.properties.millage.replace(".00", "").replace("$", "").replace(",", "")
-			);
-
 		function header() {
 			return (
 				"<h3>" + thisMarker.park.name + "</h3>" +
 				"<p>" + thisMarker.type + "</p>"
 				);
 			}
-		var oldSetPopup = thisMarker.setPopupContent;
-		function newSetPopup(long) {
+		thisMarker.setLongPopupContent = function(long) {
 			var msg = header();
 			if (long) {msg += "description of improvements would go here";}
-			oldSetPopup.call(thisMarker, msg);
+			thisMarker.setPopupContent(msg);
 			}
-		thisMarker.setPopupContent = newSetPopup;
 		thisMarker.type = feature.properties.type + " " + feature.properties.leisure;
 		thisMarker.bindPopup(header(), {closeButton: false, maxHeight: 300});
-		
 		markers.push(thisMarker);
-		
 		}
-	
 	}
 
 
@@ -243,19 +271,16 @@ function makeParkList() {
 		parklist.appendChild(li);
 		
 		for (i2 = 0; i2 < neighborhoods.length; i2++) {
-		    var polyPoints = neighborhoods[i2].shape.getLatLngs();       
+		    var polyPoints = neighborhoods[i2].getLatLngs();       
 		    var x = thisMarker.getLatLng().lat, y = thisMarker.getLatLng().lng;
-		
 		    var inside = false;
 		    for (var i3 = 0, j3 = polyPoints.length - 1; i3 < polyPoints.length; j3 = i3++) {
 		        var xi = polyPoints[i3].lat, yi = polyPoints[i3].lng;
 		        var xj = polyPoints[j3].lat, yj = polyPoints[j3].lng;
-		
 		        var intersect = ((yi > y) != (yj > y)) && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
 		        if (intersect) inside = !inside;
 		    	}
-		
-		    if (inside) {neighborhoods[i2].dollars += thisMarker.dollars;}
+		    if (inside) {neighborhoods[i2].feature.properties.money += thisMarker.money;}
 			}
 		
 		}  
@@ -273,7 +298,7 @@ function clickPark(e, open) {
 			}	
 		}
 	else {
-		markers[index].setPopupContent(false);
+		markers[index].setLongPopupContent(false);
 		}
 	}
 	
@@ -288,12 +313,30 @@ function liPark(index) {
 	}
 
 function markerLabel(type) {
-	return "<img src='" + srcFromMarkerType(type) + "' class='small'>" + type + " Parks";
+	return "<img src='" + srcFromMarkerType(type) + "' class='small'>" + type;
 	}
 
 function moneyClicked(index) {
-	markers[index].setPopupContent(true);
+	markers[index].setLongPopupContent(true);
 	pop(index);
+	}
+
+function overlayChanged(e, show) {
+	if (e.name == "Neighborhoods") {
+		if (show) {
+			legend.addTo(baseMap.map);
+			info.addTo(baseMap.map);
+			}
+		else {
+			legend.removeFrom(baseMap.map);
+			info.removeFrom(baseMap.map);
+			}
+		}
+	else {
+		overlayLayers[e.name].eachLayer(function(layer) {
+			liPark(layer.index).style.display = show ? "block" : "none";
+			});
+		}
 	}
 
 function pop(index) {
