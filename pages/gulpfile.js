25c43ad16241c3e71e32b845646c4e365fa0b37e
CJ Davis <Christopher.Davis@cqlcorp.com>
2016-04-20 12:50:51 -0400

    Use gulp to create minified stylesheet

--- pages/gulpfile.js
+++ pages/gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
     plumber = require('plumber'),
     gulp_concat = require('gulp-concat'),
-    gulp_uglify = require('gulp-uglify');
+    gulp_uglify = require('gulp-uglify'),
+    gulp_uglifycss = require('gulp-uglifycss');
 
 gulp.task('build:js', function() {
   return gulp.src([
@@ -9,5 +10,15 @@ gulp.task('build:js', function() {
   ])
     .pipe(gulp_concat('app.min.js'))
     .pipe(gulp_uglify())
+    .pipe(gulp.dest('./'));
+});
+
+
+gulp.task('build:css', function() {
+  return gulp.src([
+    '_src/css/**/*.css'
+  ])
+    .pipe(gulp_concat('styles.min.css'))
+    .pipe(gulp_uglifycss())
     .pipe(gulp.dest('./'));
 });
